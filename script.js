69c65c5e18f1e1681f11266e528d79ea9db6744f
minu0913 <minu.park9@gmail.com>
2026-03-11 20:54:04 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 
+const btn = document.getElementById('color-button');
+
+
+btn.addEventListener('click', function() {
+
+    if (document.body.style.backgroundColor === 'black') {
+        document.body.style.backgroundColor = 'white';
+        document.body.style.color = 'black';
+    } else {
+        document.body.style.backgroundColor = 'black';
+        document.body.style.color = 'white';
+    }
+});
